14aac7e3872ef8638d7b229a03fc61ddddd878f2
DylanLale <disneygamer706@gmail.com>
2022-10-03 08:57:47 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Part_6___Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
